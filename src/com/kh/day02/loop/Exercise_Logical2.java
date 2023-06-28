b7de70fc1f608c118dbd19442852346c0a8a33fc
SONGJUNGYUNG <cjswofhxh123@gmail.com>
2023-06-28 09:14:29 +0900

    [ADD] 23/06/28 연산자, 반복문 예제 및 실습 1.논리연산자 문제 풀가 2. 반복문 -for,if,while

---
+++ src/com/kh/day02/loop/Exercise_Logical2.java
@@ -0,0 +1,22 @@
+package com.kh.day02.loop;
+
+import java.util.Scanner;
+
+public class Exercise_Logical2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//입력받은 문자가 대문자인지 소문자인지 확인하세요!
+		//변수 word에 초기화
+		//영어 대문자 확인 : true
+		Scanner sc = new Scanner(System.in);
+		System.out.println("문자를 입력해주세요: ");
+//		String words = sc.next();
+//		//문자열에서 첫번째 인덱스를 자르니까 문자가 됨
+//		System.out.println("문자열 확인 : " + words);
+		char word = sc.next().charAt(0);
+		System.out.println("문자 출력 : " + word);
+		boolean result  = (word > 'A') && (word <= 90);
+		System.out.println("영어 대문자 확인 : " +result);
+	}
+}
---
+++ src/com/kh/day02/loop/exercise/Examcise_ForDouble1.java
@@ -0,0 +1,16 @@
+package com.kh.day02.loop.exercise;
+
+public class Examcise_ForDouble1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//구구단
+			for(int k = 2; k<10; k++) {
+				System.out.println(k +"단");				
+			for(int h = 1; h <10; h++) {
+				System.out.print(k + " * " + h+ " = " + k*h);
+			}
+		}
+	}
+}
